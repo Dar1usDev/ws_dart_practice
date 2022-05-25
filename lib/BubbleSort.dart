5d626a2a88939488b399492f1b12ccaf510d6851
Дмитрий Селезнев <Selesnjow@gmail.com>
2022-05-25 14:27:32 +1000

    1337

--- lib/BubbleSort.dart
+++ lib/BubbleSort.dart
@@ -9,101 +9,154 @@ class BubbleSortPage extends StatefulWidget {
   State<BubbleSortPage> createState() => _BubbleSortPageState();
 }
 
-enum DataType {
-  intType,
-  doubleType,
-  stringType
-}
+enum DataType { intType, doubleType, stringType }
 
 class _BubbleSortPageState extends State<BubbleSortPage> {
+  DataType _currentType = DataType.intType;
 
-  int dataType = 0;
+  String _unsortedArrayStr = 'source';
+  String _sortedArrayStr = 'output';
 
-  DataType? _currentType = DataType.intType;
-
-  String _unsortedArray = 'source';
+  late List<Object> _unsortedArray;
+  late List<Object> _sortedArray;
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Column(
-          textDirection: TextDirection.ltr,
-          children: [
-            Column(
-              children:
-              [
-                ListTile(
-                  tileColor: Colors.blue,
-                  title: const Text('int'),
-                  leading: Radio<DataType>(
-                    value: DataType.intType,
-                    groupValue: _currentType,
-                    onChanged: (DataType? value) {
-                      setState(() {
-                        _currentType = value;
-                      });
+      child: Column(
+        textDirection: TextDirection.ltr,
+        children: [
+          Column(
+            children: [
+              ListTile(
+                tileColor: Colors.blue,
+                title: const Text('int'),
+                leading: Radio<DataType>(
+                  value: DataType.intType,
+                  groupValue: _currentType,
+                  onChanged: (DataType? value) {
+                    setState(() {
+                      _currentType = value!;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                tileColor: Colors.lime,
+                title: const Text('double'),
+                leading: Radio<DataType>(
+                  value: DataType.doubleType,
+                  groupValue: _currentType,
+                  onChanged: (DataType? value) {
+                    setState(() {
+                      _currentType = value!;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                tileColor: Colors.orange,
+                title: const Text('string'),
+                leading: Radio<DataType>(
+                  value: DataType.stringType,
+                  groupValue: _currentType,
+                  onChanged: (DataType? value) {
+                    setState(() {
+                      _currentType = value!;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                _unsortedArrayStr,
+              ),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () {
+                      _randomData(_currentType);
                     },
-                  ),
-                ),
-                ListTile(
-                  tileColor: Colors.lime,
-                  title: const Text('double'),
-                  leading: Radio<DataType>(
-                    value: DataType.doubleType,
-                    groupValue: _currentType,
-                    onChanged: (DataType? value) {
-                      setState(() {
-                        _currentType = value;
-                      });
-                    },
-                  ),
-                ),
-                ListTile(
-                  tileColor: Colors.orange,
-                  title: const Text('string'),
-                  leading: Radio<DataType>(
-                    value: DataType.stringType,
-                    groupValue: _currentType,
-                    onChanged: (DataType? value) {
-                      setState(() {
-                        _currentType = value;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                    _unsortedArray,
-                ),
-                ElevatedButton(
-                    onPressed: _randomData(dataType),
-                    child: const Text("Randomize")
-                )
-              ],
-            ),
-            ElevatedButton(
-                onPressed: _sort(),
-                child: const Text("Sort")
-            ),
-            const Text(
-                "Output"
-            ),
-          ],
-        ),
+                    child: const Text("Randomize")),
+              )
+            ],
+          ),
+          ElevatedButton(
+              onPressed: () {
+                _sort();
+              },
+              child: const Text("Sort")),
+          Text(
+            _sortedArrayStr,
+          ),
+        ],
+      ),
     );
   } // build()
 
-  _randomData(int dataType) {
+  _randomData(DataType dataType) {
+    const _chars =
+        'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+    Random _rnd = Random();
+
+    String getRandomString(int length) =>
+        String.fromCharCodes(Iterable.generate(
+            length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
     setState(() {
-      _unsortedArray = Random().nextInt(100).toString();
+      _unsortedArray = [];
+      for (int i = 0; i < 10; i++) {
+        switch (dataType) {
+          case DataType.intType:
+            _unsortedArray.add(Random().nextInt(30));
+            break;
+          case DataType.doubleType:
+            _unsortedArray
+                .add((Random().nextDouble() * 30).toStringAsPrecision(3));
+            break;
+          case DataType.stringType:
+            _unsortedArray.add(getRandomString(2));
+            break;
+        }
+      }
+
+      _unsortedArrayStr = '';
+      _sortedArrayStr = '';
+
+      for (int i = 0; i < 10; i++) {
+        _unsortedArrayStr += i == 0 ? '' : ' ';
+        _unsortedArrayStr += _unsortedArray[i].toString();
+      }
     });
-    //throw Exception();
   }
 
   _sort() {
+    setState(() {
+      _sortedArrayStr = '';
 
+      _sortedArray = _unsortedArray;
+
+      for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < i; j++) {
+          if (_sortedArray[j]
+              .toString()
+              .compareTo(_sortedArray[j + 1].toString()) >
+              1) {
+            Object _temp = _sortedArray[j];
+            _sortedArray[j] = _sortedArray[j + 1];
+            _sortedArray[j + 1] = _temp;
+          }
+        }
+      }
+
+      for (int i = 0; i < 10; i++) {
+        _sortedArrayStr += i == 0 ? '' : ' ';
+        _sortedArrayStr += _sortedArray[i].toString();
+      }
+    });
   }
 }
